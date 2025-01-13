a572aaadb595df3324cd89384569c0387c298d0b
Alexander Tarasenko <tarasenkoas@gmail.com>
2025-01-14 00:51:29 +0300

    skip-envsubst

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,21 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 if [ -f /etc/nginx/nginx.template.conf ]; then
   cp -f /etc/nginx/nginx.template.conf /etc/nginx/nginx.conf.tmp
-  IFS=$'\n'
-  for K in $(printenv | cut -d= -f1); do
-    echo "$0 - envsubst for $K ..."
-    cat /etc/nginx/nginx.conf.tmp | envsubst '${'$K'}' | sponge /etc/nginx/nginx.conf.tmp
-  done
+  if [ -n "$ENVSUBST_ENABLED" ]; then
+    echo "notice: doing envsubst ..."
+    IFS=$'\n'
+    for K in $(printenv | cut -d= -f1); do
+      echo "$0 - envsubst for $K ..."
+      cat /etc/nginx/nginx.conf.tmp | envsubst '${'$K'}' | sponge /etc/nginx/nginx.conf.tmp
+    done
+  else
+    echo "notice: ENVSUBST_ENABLED env var is missing, skip envsubst"
+  fi
   mv -f /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 fi
 
 /prometheus-nginx-socket-exporter &
 
 exec "$@"
+
